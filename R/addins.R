7baf118b387b57dac4c43a888bdfbd2c43aa8512
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2019-10-19 13:11:02 -0400

    live preview addin now renders Rmd as well

--- R/addins.R
+++ R/addins.R
@@ -1,13 +1,31 @@
-live_preview <- function(path = getwd(), pattern = "[.](js|css|html?|s[ca]ss)$", ..., external = FALSE) {
+live_preview <- function(
+  path = getwd(),
+  pattern = "[.](js|css|[Rr][Mm][Dd]|html?|s[ca]ss)$",
+  ...,
+  external = FALSE
+) {
   requires_pkg("servr")
   path_dir <- dirname(path)
-  path_file <- basename(path)
+  path_file <- if (grepl("[.]rmd", path, ignore.case = TRUE)) "/" else basename(path)
   viewer <- if (external) {
     utils::browseURL
   } else {
     getOption("viewer", utils::browseURL)
   }
-  x <- servr::httw(path_dir, pattern = pattern, initpath = path_file, browser = FALSE, ...)
+  render <- function(path) {
+    if (grepl("[.]rmd", path, ignore.case = TRUE)) {
+      rmarkdown::render(path, envir = new.env())
+    }
+    path
+  }
+  x <- servr::httw(
+    dir = path_dir,
+    pattern = pattern,
+    initpath = path_file,
+    browser = FALSE,
+    handler = render,
+    ...
+  )
   viewer(x$url)
 }
 
