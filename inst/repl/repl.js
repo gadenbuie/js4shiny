44a9726c4a7c07752b8b731ec8afa4fe02a16acd
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2019-10-30 23:09:26 -0400

    eslint --fix

--- inst/repl/repl.js
+++ inst/repl/repl.js
@@ -4,18 +4,23 @@ $(document).ready(function() {
     document.getElementById(id).textContent = '';
   };
 
-  document.getElementById("clear-log")
-    .addEventListener("click", () => clearElementById('log'));
+  document.getElementById('clear-log').addEventListener('click', () => {
+    clearElementById('log');
+  });
 
   document.getElementById('hide-log').addEventListener('click', () => {
     const panelCode = document.querySelector('.panel-code');
-    const isConsoleMinimized = panelCode.classList.contains('console__minimized');
+    const isConsoleMinimized = panelCode.classList.contains(
+      'console__minimized'
+    );
     const newBtnText = isConsoleMinimized ? 'Hide' : 'Show';
     panelCode.classList.toggle('console__minimized');
     setTimeout(() => {
       // Resize editors to avoid clipping issues in Chrome
       const editors = ['code_js', 'code_css', 'code_md'];
-      editors.forEach(id => ace.edit(id).resize())
+      editors.forEach((id) => {
+        ace.edit(id).resize();
+      });
     }, 0);
     const hideBtn = document.getElementById('hide-log');
     hideBtn.innerHTML = newBtnText;
@@ -23,22 +28,23 @@ $(document).ready(function() {
   });
 
   // $('#example').on('change', () => { $("#log").text(''); })
-  document.getElementById("example")
-    .addEventListener("change", () => clearElementById('log'));
+  document.getElementById('example').addEventListener('change', () => {
+    clearElementById('log');
+  });
 
   // scroll console log to bottom on update
-  document.getElementById('log').addEventListener('consoleLog', e => {
-    e.target.scrollTop = e.target.scrollHeight
+  document.getElementById('log').addEventListener('consoleLog', (e) => {
+    e.target.scrollTop = e.target.scrollHeight;
   });
 
   function showSolutionButton(state) {
     if (state) {
-      $("#show_solution").show();
+      $('#show_solution').show();
     } else {
-      $("#show_solution").hide();
+      $('#show_solution').hide();
     }
   }
 
   Shiny.addCustomMessageHandler('clearElementById', clearElementById);
-  Shiny.addCustomMessageHandler("showSolutionButton", showSolutionButton);
+  Shiny.addCustomMessageHandler('showSolutionButton', showSolutionButton);
 });
