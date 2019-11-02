5e229b864f2384929f5b0d94f21d581049efd48a
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2019-11-02 15:19:18 -0400

    minor housekeeping

--- R/repl.R
+++ R/repl.R
@@ -254,7 +254,7 @@ repl_ui <- function(
         ),
         if (!js_repl_only) shiny::div(
           class = "resize-handle resize--horizontal__top",
-          HTML('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 36 36"><path d="M20 9H4v2h16V9zM4 15h16v-2H4v2z"></path></svg>')
+          shiny::HTML('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 36 36"><path d="M20 9H4v2h16V9zM4 15h16v-2H4v2z"></path></svg>')
         ),
         shiny::div(
           class = "panel-html",
