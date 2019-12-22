cbd0f3ca5b37f69f579100fd3cfc39051145fe93
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2019-12-22 15:21:18 -0500

    helper function to escape inline code examples of html

--- R/utils.R
+++ R/utils.R
@@ -18,3 +18,11 @@ collapse <- function(..., sep_c = '\n') paste(..., collapse = sep_c)
 collapse0 <- function(..., sep_c = "\n") paste(..., sep = "", collapse = sep_c)
 
 read_lines <- function(path, ..., warn = FALSE) readLines(path, warn = warn, ...)
+
+escape_html_example <- function(...) {
+  x <- list(...)
+  x <- purrr::map_chr(x, ~ if (inherits(.x, "AsIs")) paste(.x) else htmltools::htmlEscape(.x))
+  x <- gsub("\n", "<br>", x)
+  x <- gsub(" ", "&nbsp;", x)
+  collapse(x, sep_c = "")
+}
