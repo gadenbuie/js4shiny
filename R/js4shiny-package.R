e5f0b63a1d45074a8b62bbc37066e9d923075506
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2019-12-26 16:12:22 -0500

    document first sentences dataset

--- R/js4shiny-package.R
+++ R/js4shiny-package.R
@@ -7,3 +7,17 @@
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
+
+
+#' First Sentences of Books
+#'
+#' First sentences of books, downloaded from
+#' <https://firstsentencesofbooks.tumblr.com/>.
+#'
+#' @format A data frame with 40 rows and 3 variables:
+#' \describe{
+#'   \item{\code{quote}}{The first sentence of the book}
+#'   \item{\code{title}}{The title of the book}
+#'   \item{\code{author}}{The author of the book}
+#' }
+"first_sentences"
