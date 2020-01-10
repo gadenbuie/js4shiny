f8e1373469c46fbb2ec0a407e7934aa8de426f41
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-01-10 17:49:06 -0500

    :sleepy: maybe this is what's missing?

--- tic.R
+++ tic.R
@@ -4,5 +4,8 @@ do_package_checks()
 build_pkgdown <- ci_has_env("BUILD_PKGDOWN")
 if (ci_on_travis() && build_pkgdown) {
   # creates pkgdown site and pushes to gh-pages branch
+  get_stage("before_deploy") %>%
+    add_step(step_setup_ssh())
+
   tic::do_pkgdown(deploy = TRUE)
 }
