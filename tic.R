7ae17b6b153171a788fbe515fab46860f886edf4
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-01-10 17:35:33 -0500

    :construction_worker: try again with the pkgdown

--- tic.R
+++ tic.R
@@ -1,7 +1,7 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
 do_package_checks()
 
-build_pkgdown <- isTRUE(ci_get_env("BUILD_PKGDOWN"))
+build_pkgdown <- ci_has_env("BUILD_PKGDOWN")
 if (ci_on_travis() && build_pkgdown) {
   # creates pkgdown site and pushes to gh-pages branch
   tic::do_pkgdown(deploy = TRUE)
