634bd5c40be8e953e2b8d51854507fad744b73c3
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-01-10 22:32:45 -0500

    starting over with travis

--- tic.R
+++ tic.R
@@ -1,13 +1,7 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
 do_package_checks()
 
-build_pkgdown <- ci_has_env("BUILD_PKGDOWN")
-if (ci_on_travis() && build_pkgdown) {
+if (ci_on_travis()) {
   # creates pkgdown site and pushes to gh-pages branch
-  get_stage("before_deploy") %>%
-    add_step(step_install_ssh_keys()) %>%
-    add_step(step_add_to_known_hosts()) %>%
-    add_step(step_test_ssh())
-
-  do_pkgdown(deploy = TRUE)
+  do_pkgdown()
 }
