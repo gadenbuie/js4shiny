055fba0ac8f28526f04ac3e5a66232fdcf76ec1d
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-01-11 17:08:57 -0500

    back to id_rsa

--- tic.R
+++ tic.R
@@ -5,6 +5,7 @@ if (ci_on_travis() && ci_has_env("BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
   print(tic::ci_can_push("TRAVIS_DEPLOY_KEY"))
   get_stage("before_deploy") %>%
-    add_step(step_test_ssh())
+    add_step(step_setup_ssh("id_rsa"))
+
   do_pkgdown(deploy = TRUE)
 }
