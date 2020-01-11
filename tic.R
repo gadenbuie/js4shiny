045011af4f4d64a3bbba9bac3afc3eb43d2aff18
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-01-10 22:14:26 -0500

    why does it keep skipp step_setup_ssh()?

--- tic.R
+++ tic.R
@@ -4,5 +4,10 @@ do_package_checks()
 build_pkgdown <- ci_has_env("BUILD_PKGDOWN")
 if (ci_on_travis() && build_pkgdown) {
   # creates pkgdown site and pushes to gh-pages branch
-  tic::do_pkgdown(deploy = TRUE)
+  get_stage("before_deploy") %>%
+    add_step(step_install_ssh_keys()) %>%
+    add_step(step_add_to_known_hosts()) %>%
+    add_step(step_test_ssh())
+
+  do_pkgdown(deploy = TRUE)
 }
