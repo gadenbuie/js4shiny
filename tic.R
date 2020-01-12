9c6911f547d642bd7ccc9baa35e09f5f45846397
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-01-11 20:40:40 -0500

    :rewind: go back to last good travis config

--- tic.R
+++ tic.R
@@ -1,11 +1,10 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
 do_package_checks()
 
-if (ci_on_travis() && ci_has_env("BUILD_PKGDOWN")) {
+if (ci_on_travis()) {
   # creates pkgdown site and pushes to gh-pages branch
-  print(tic::ci_can_push("TRAVIS_DEPLOY_KEY"))
   get_stage("before_deploy") %>%
     add_step(step_setup_ssh("id_rsa"))
 
-  do_pkgdown(deploy = TRUE)
+  do_pkgdown()
 }
