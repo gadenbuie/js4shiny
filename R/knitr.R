b3eeb80966384888ab34e08656766d79c2127819
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-01-18 22:19:08 -0500

    Add html_raw to html knitr engine chunk option

--- R/knitr.R
+++ R/knitr.R
@@ -175,8 +175,11 @@ knitr_json_engine <- function() {
 knitr_html_engine <- function() {
   function(options) {
     out <- if (options$eval && knitr::is_html_output()) {
+      # use pandoc's raw html block by default, but this isn't always available
+      # so it can be disabled with the html_raw chunk option.
+      raw <- isTRUE(options$html_raw %||% TRUE)
       paste0(
-        '\n```{=html}\n',
+        if (raw) '\n```{=html}\n',
         '<div',
         paste0(' id="out-', gsub("[^a-zA-Z0-9_.]", "-", options$label), '"'),
         if (!is.null(options$class.output)) paste0(
@@ -184,7 +187,8 @@ knitr_html_engine <- function() {
         ),
         '>\n',
         paste(options$code, collapse = "\n"),
-        "\n</div>\n```"
+        "\n</div>",
+        if (raw) "\n```"
       )
     }
     options$results <- "asis"
