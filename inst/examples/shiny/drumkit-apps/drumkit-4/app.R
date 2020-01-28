2db9f1636c8be136d7f17892f3210364eadf9127
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-01-27 20:48:04 -0800

    v0.0.28
    Fixes first drumkit example

--- inst/examples/shiny/drumkit-apps/drumkit-4/app.R
+++ inst/examples/shiny/drumkit-apps/drumkit-4/app.R
@@ -1,33 +1,20 @@
 library(shiny)
 
-ui <- fluidPage(
-  titlePanel("htlmwidgets rock"),
-  sidebarLayout(
-    sidebarPanel(
-      # Sidebar with a slider for number of bars
-      sliderInput("n_bars", "Number of bars", 1, 26, 10),
-      # And a button to generate new data
-      actionButton("new_data", "New Data")
-    ),
-    mainPanel(
-      # The widget output goes here with <widget>Output()
-    )
+drumkitUI <- function(id) {
+  ns <- NS(id)
+  tagList(
+
   )
-)
-
-server <- function(input, output) {
-  data <- reactive({
-    # Re-generate data on button click or when the number of bars changes
-    input$new_data
-
-    data.frame(
-      x = LETTERS[seq_len(input$n_bars)],
-      Frequency = runif(input$n_bars)
-    )
-  })
-
-  # Render the widget here with render<widget>(<widget>)
-  # output$chart <- ....
 }
 
-shinyApp(ui = ui, server = server)
+drumkit <- function(input, output, session, ...) {
+}
+
+ui <- fluidPage(
+)
+
+server <- function(input, output, session) {
+
+}
+
+shinyApp(ui, server)
